9b2c3a257fe0f5f0d2f443663c23da05dd13cf30
Skyler Beckman <sbeckman@purdue.edu>
2012-11-17 23:46:32 -0500

    Added Landscape view for movie

--- src/edu/purdue/cs/movierec/ActivityMovie.java
+++ src/edu/purdue/cs/movierec/ActivityMovie.java
@@ -4,17 +4,22 @@ import java.io.InputStream;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Log;
+import android.view.Display;
 import android.view.KeyEvent;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ActivityMovie extends Activity {
 	
-	MovieApp appState;	
+	MovieApp appState;
+	ActionBar actionBar;
 	Movie movie;
 	
 	private TextView movieTitle;
@@ -23,30 +28,79 @@ public class ActivityMovie extends Activity {
 	private TextView movieGenres;
 	private TextView movieDirectors;
 	private ImageView fanArt;
+	private ImageView coverArt;
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+			landscape();
+		} else if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+			portrait();
+		}
+	}
+	
+	public void landscape() {
+		setContentView(R.layout.movie_landscape);
+		
+		movieSubTitle = (TextView)findViewById(R.id.movie_subtitle_landscape);
+		coverArt = (ImageView)findViewById(R.id.coverArtMovie);
+		movieGenres = (TextView)findViewById(R.id.genres_landscape);
+		movieActors = (TextView)findViewById(R.id.actors_landscape);
+		movieDirectors = (TextView)findViewById(R.id.directors_landscape);
+		
+		actionBar.setTitle(movie.getTitle());
+		
+		coverArt.setImageBitmap(movie.getCoverart());
+    	movieSubTitle.setText(getSubtitle()); 
+    	movieGenres.setText(getGenres());
+    	movieActors.setText(getActors());
+    	movieDirectors.setText(getDirectors());
+    	
+	}
+	
+	public void portrait() {
         setContentView(R.layout.movie);
-        
-        appState = (MovieApp)getApplicationContext();
-        movie =  appState.getMovie();
         
         movieTitle = (TextView)findViewById(R.id.movie_title);
         movieSubTitle = (TextView)findViewById(R.id.movie_subtitle);
         movieActors = (TextView)findViewById(R.id.actors); 
         movieGenres = (TextView)findViewById(R.id.genres);
         movieDirectors = (TextView)findViewById(R.id.directors);
-        fanArt = (ImageView)findViewById(R.id.fanart);        
         
+        actionBar.setTitle(R.string.app_name);
         
-        //Send fanart from server or download through url?????
-    	//fanArt.setImageResource(R.drawable.prometheus);
     	new DownloadImageTask((ImageView) findViewById(R.id.fanart)).execute("http://cf2.imgobject.com/t/p/original/qfXeiDyuvLgfXqypnP5e3t8mxV4.jpg");
-        	
-        movieTitle.setText(movie.getTitle());
+    	movieTitle.setText(movie.getTitle());    
+    	movieSubTitle.setText(getSubtitle());                   
+        movieGenres.setText(getGenres());        
+        movieActors.setText(getActors());        
+        movieDirectors.setText(getDirectors());
         
-        StringBuilder subtitle = new StringBuilder();
+	}
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WindowManager mWindowManager = (WindowManager)getSystemService(WINDOW_SERVICE);
+        Display mDisplay = mWindowManager.getDefaultDisplay();
+        
+        appState = (MovieApp)getApplicationContext();
+        movie =  appState.getMovie(); 
+        
+        actionBar = this.getActionBar();
+        
+        if(mDisplay.getRotation() > 0) {
+        	landscape();
+        } else {
+        	portrait();
+        }    
+    }
+    
+    public String getSubtitle() {
+    	StringBuilder subtitle = new StringBuilder();
         switch(movie.getMPAA()) {
         	case R.drawable.g: subtitle.append("G, "); break; //G
         	case R.drawable.pg: subtitle.append("PG, "); break; //PG
@@ -58,34 +112,43 @@ public class ActivityMovie extends Activity {
         	case R.drawable.ur: subtitle.append("UR, "); break; 
         	case R.drawable.nr: subtitle.append("NR, "); break;
         }
-        subtitle.append(movie.getYear());        
-        movieSubTitle.setText(subtitle.toString());
+        subtitle.append(movie.getYear());   
         
-        //Genres
-        StringBuilder genres = new StringBuilder();
-        genres.append(movie.getGenres().get(0));
-        for(int i = 1; i < movie.getGenres().size(); i++) {
-        	genres.append(", " + movie.getGenres().get(i));
-        }
-        movieGenres.setText(genres.toString());
-        
-        //Actors
+        return subtitle.toString();
+    }
+    
+    public String getActors() {
+    	//Actors
         StringBuilder actors = new StringBuilder();
         actors.append(movie.getActors().get(0));
         for(int i = 1; i < movie.getActors().size(); i++) {
         	actors.append(", " + movie.getActors().get(i));
         }
-        movieActors.setText(actors.toString());
         
-        //Directors
+        return actors.toString();
+    }
+
+    public String getDirectors() {
+    	//Directors
         StringBuilder directors = new StringBuilder();
         directors.append(movie.getDirectors().get(0));
         for(int i = 1; i < movie.getDirectors().size(); i++) {
         	directors.append(", " + movie.getDirectors().get(i));
         }
-        movieDirectors.setText(directors.toString());
+        
+        return directors.toString();
     }
-
+    
+    public String getGenres() {
+    	//Genres
+        StringBuilder genres = new StringBuilder();
+        genres.append(movie.getGenres().get(0));
+        for(int i = 1; i < movie.getGenres().size(); i++) {
+        	genres.append(", " + movie.getGenres().get(i));
+        }
+        
+        return genres.toString();
+    }
     
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
